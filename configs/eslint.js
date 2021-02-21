d715def680b98861a1d20180336cb6bcf7cbcc08
Rafael Pedicini <rafael@rafgraph.dev>
2021-02-21 17:49:07 -0500

    Update dependencies

--- configs/eslint.js
+++ configs/eslint.js
@@ -26,8 +26,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/react',
   ],
   settings: {
     react: {
