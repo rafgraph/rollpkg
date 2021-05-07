ee6eafc845420776093c9a72d647cbea10a6f766
Rafael Pedicini <rafael@rafgraph.dev>
2021-05-07 16:25:54 -0400

    Make typescript eslint rules more forgiving

--- configs/eslint.js
+++ configs/eslint.js
@@ -38,6 +38,13 @@ module.exports = {
     // see for rational https://basarat.gitbook.io/typescript/main-1/defaultisbad
     'import/no-default-export': 'error',
     '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-unsafe-call': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
+    '@typescript-eslint/no-unsafe-return': 'warn',
+    '@typescript-eslint/no-this-alias': 'warn',
   },
   overrides: [
     {
